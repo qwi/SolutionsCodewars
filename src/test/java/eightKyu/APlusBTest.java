6f13d7cfdb92ff547d9bc4a2466c2a0cbd01bf51
Chemerisov oleg <ochemerisov@cinimex.ru>
2020-06-18 23:07:08 +0300

    APlusB 1. Added a task that produces a sum of 2 numbers of type "byte" 2. Added solution for her 3. Added unit test

---
+++ src/test/java/eightKyu/APlusBTest.java
@@ -0,0 +1,20 @@
+package eightKyu;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * APlusBTest.
+ *
+ * @author Oleg_Chemerisov
+ */
+public class APlusBTest {
+
+    @Test
+    public void testSum() throws Exception {
+        byte a = 1;
+        byte b = 2;
+        assertEquals(3, APlusB.sum(a, b));
+    }
+}
